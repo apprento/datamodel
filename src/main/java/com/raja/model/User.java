315893c97d38e4c0a0b458b171c8a4cf2f82c9d4
raja <apprentoapplication@gmail.com>
2017-03-07 12:12:31 +0530

    added entites

---
+++ src/main/java/com/raja/model/User.java
@@ -0,0 +1,43 @@
+package com.raja.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id", unique=true)
+	private Long id;
+	
+	@Column(unique= true)
+	private String userName;
+	
+	@Column
+	private String password;
+	
+	@Column
+	private String fullName;
+	
+	@Column
+	private String number;
+	
+	@Column
+	private String emailId;
+
+}
